359f5a9ab473607550800e37dc745aec3d06ffa3
Timur-O <19150752+Timur-O@users.noreply.github.com>
2020-12-31 01:13:00 +0100

    Add css + js files for admin

---
+++ AffiliateAdminDashboard/js/script.js
@@ -0,0 +1,18 @@
+// Initialize Materialize Scripts
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+
+  // Fix for the footer
+  if (document.location.pathname != rootOfFiles) {
+    var currPage = document.location.pathname.match(/[^\/]+$/)[0];
+  } else {
+    $('footer').attr('class', 'footerlogin');
+  }
+
+  switch (currPage) {
+    case 'index':
+    case 'index.php':
+      $('footer').attr('class', 'footerlogin');
+      break;
+  }
+});
