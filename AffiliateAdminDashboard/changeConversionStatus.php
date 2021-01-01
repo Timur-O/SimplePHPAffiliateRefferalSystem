2cef415b3cf92a6ea029e8b3617c7c5e0be59bf4
Timur-O <19150752+Timur-O@users.noreply.github.com>
2021-01-01 16:53:04 +0100

    Bug fix

--- AffiliateAdminDashboard/changeConversionStatus.php
+++ AffiliateAdminDashboard/changeConversionStatus.php
@@ -4,6 +4,14 @@ if (isset($_POST['approveConversion']) && isset($_SESSION['userRefCode'])) {
     include 'config.php';
     $sql = "UPDATE `{$conversionsTableName}` SET `approved` =  1 WHERE `conversionID` = {$_POST['conversionID']}";
     $result = $conn->query($sql);
+
+    $sql2 = "SELECT `commissionAmount` FROM `{$conversionsTableName}` WHERE  `conversionID` = {$_POST['conversionID']}";
+    $result = $conn->query($sql2)->fetch_assoc();
+    $commissionAmount = $result['commissionAmount '];
+
+    $sql3 = "UPDATE `{$affiliatesTableName}` SET `commissionBalance ` =  `commissionBalance ` + {$commissionAmount} WHERE `affiliateID` = {$_SESSION['userRefCode']}";
+    $result = $conn->query($sql3);
+
     header("Location: conversions.php");
     die();
 } else if (isset($_POST['rejectConversion']) && isset($_SESSION['userRefCode'])) {
